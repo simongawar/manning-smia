a6d10d49546c3f0d70ac605d791f4bbe217954e6
Simon Gawar Dak <gawardak@arizona.edu>
2025-10-19 13:15:53 +0200

    update LicenseController file

--- chapter7/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ chapter7/licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -26,54 +26,58 @@ import com.optimagrowth.license.utils.UserContextHolder;
 @RestController
 @RequestMapping(value="v1/organization/{organizationId}/license")
 public class LicenseController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(LicenseController.class);
+    
+    private static final Logger logger = LoggerFactory.getLogger(LicenseController.class);
 
-	@Autowired
-	private LicenseService licenseService;
+    @Autowired
+    private LicenseService licenseService;
 
-	@RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
-	public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
-			@PathVariable("licenseId") String licenseId) {
+    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
+    public ResponseEntity<License> getLicense( @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
 
-		License license = licenseService.getLicense(licenseId, organizationId, "");
-		license.add( 
-				linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
-				linkTo(methodOn(LicenseController.class).createLicense(license)).withRel("createLicense"),
-				linkTo(methodOn(LicenseController.class).updateLicense(license)).withRel("updateLicense"),
-				linkTo(methodOn(LicenseController.class).deleteLicense(license.getLicenseId())).withRel("deleteLicense")
-				);
+        License license = licenseService.getLicense(licenseId, organizationId, "");
+        license.add( 
+                linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+                linkTo(methodOn(LicenseController.class).createLicense(organizationId, license)).withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
+                // FIX: Added organizationId to the methodOn call for correct URL building
+                linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+                );
 
-		return ResponseEntity.ok(license);
-	}
+        return ResponseEntity.ok(license);
+    }
 
-	@RequestMapping(value="/{licenseId}/{clientType}",method = RequestMethod.GET)
-	public License getLicensesWithClient( @PathVariable("organizationId") String organizationId,
-			@PathVariable("licenseId") String licenseId,
-			@PathVariable("clientType") String clientType) {
+    @RequestMapping(value="/{licenseId}/{clientType}",method = RequestMethod.GET)
+    public License getLicensesWithClient( @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId,
+            @PathVariable("clientType") String clientType) {
 
-		return licenseService.getLicense(licenseId, organizationId, clientType);
-	}
+        return licenseService.getLicense(licenseId, organizationId, clientType);
+    }
 
-	@PutMapping
-	public ResponseEntity<License> updateLicense(@RequestBody License request) {
-		return ResponseEntity.ok(licenseService.updateLicense(request));
-	}
+    // FIX: Added organizationId to the method signature to match the base path URL
+    @PutMapping
+    public ResponseEntity<License> updateLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request));
+    }
 
-	@PostMapping
-	public ResponseEntity<License> createLicense(@RequestBody License request) {
-		return ResponseEntity.ok(licenseService.createLicense(request));
-	}
+    // FIX: Added organizationId to the method signature to match the base path URL
+    @PostMapping
+    public ResponseEntity<License> createLicense(@PathVariable("organizationId") String organizationId, @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request));
+    }
 
-	@DeleteMapping(value="/{licenseId}")
-	public ResponseEntity<String> deleteLicense(@PathVariable("licenseId") String licenseId) {
-		return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
-	}
+    // FIX: Added organizationId to the method signature to match the base path URL and HATEOAS link
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId));
+    }
 
-	@RequestMapping(value="/",method = RequestMethod.GET)
-	public List<License> getLicenses( @PathVariable("organizationId") String organizationId) throws TimeoutException {
-		logger.debug("LicenseServiceController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
-		return licenseService.getLicensesByOrganization(organizationId);
-	}
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public List<License> getLicenses( @PathVariable("organizationId") String organizationId) throws TimeoutException {
+        logger.debug("LicenseServiceController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+        return licenseService.getLicensesByOrganization(organizationId);
+    }
 
 }
